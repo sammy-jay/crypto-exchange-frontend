c1594c1c849f10b56fe10c3d820e5c1e2fc7fe9c
Solomonmatt <100151674+Solomonmatt@users.noreply.github.com>
2022-11-21 14:52:23 +0100

    Add files via upload

---
+++ css/evaluation/color.php
@@ -0,0 +1,48 @@
+
+:root {
+    --main-color: #F7931A;
+}
+
+.feature-box .content {
+    box-shadow: inset 0 0px 66px #F7931A47;
+}
+
+.invest-plan-top::before {
+    box-shadow: inset 0 0px 66px #F7931Ad4;
+}
+
+.invest-plan::before {
+    box-shadow: inset 0 0px 66px #F7931A3b;
+}
+
+.invest-plan-shape::after {
+    background-color: #F7931A33;
+}
+
+.invest-plan:hover .invest-plan-shape::after {
+    background-color: #F7931A;
+}
+
+.work-box {
+    box-shadow: inset 0 0px 20px #F7931Ac4;
+}
+
+.work-box .icon {
+    box-shadow: inset 0 0px 20px #F7931Ac4;
+}
+
+.cmn-table.style-separate tbody tr {
+    box-shadow: inset 0 0px 8px #F7931A4d;
+}
+
+.testimonial-box .content {
+    box-shadow: inset 0 0px 20px #F7931Ac4;
+}
+
+.referral-box::before {
+    box-shadow: inset 0 0px 20px #F7931Ac4;
+}
+
+.referral-box-step {
+    box-shadow: inset 0 0px 6px #F7931A9e;
+}
